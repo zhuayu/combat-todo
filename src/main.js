3c242dd81c29302a758f4417ee6d970265dba7cf
Jax Chu <294242589@qq.com>
2019-04-01 20:25:52 +0800

    clear route

--- src/main.js
+++ src/main.js
@@ -1,25 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import TheTodos from '@/components/TheTodos'
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: [{
-    path: '/',
-    name: 'Root',
-    component: TheTodos,
-  },{
-    path: '/:id',
-    name: 'Filter',
-    component: TheTodos
-  }]
-})
+import router from '@/router/index'
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App),
