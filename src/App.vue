550a7e2268fc52a2fd882d0f00bba70e8d6b57c1
Jax Chu <294242589@qq.com>
2019-04-01 16:03:54 +0800

    filter

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@
     <section class="todoapp" v-cloak>
       <header class="header">
         <h1>todos</h1>
-        <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?">
+        <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addTodo()">
       </header>
       <section class="main">
         <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone">
         <label for="toggle-all">Mark all as complete</label>
         <ul class="todo-list">
-          <li :class="['todo',{completed: todo.completed === true}, {editing: editTodo === todo}]" v-for="(todo,index) in todos" :key="index">
+          <li :class="['todo',{completed: todo.completed === true}, {editing: editTodo === todo}]" v-for="(todo,index) in showTodo" :key="index">
             <div class="view">
               <input class="toggle" type="checkbox" v-model="todo.completed">
               <label @dblclick="editingMode(todo)">{{todo.title}}</label>
-              <button class="destroy"></button>
+              <button class="destroy" @click="removeTodo(todo)"></button>
             </div>
             <input class="edit" type="text" 
               v-todo-focus="editTodo === todo" 
@@ -30,11 +30,11 @@
           <strong>{{todos.length}}</strong> todo
         </span>
         <ul class="filters">
-          <li><a href="#/all" class="selected">All</a></li>
-          <li><a href="#/active">Active</a></li>
-          <li><a href="#/completed">Completed</a></li>
+          <li><a href="#/all" :class="{selected: filter === 'all'}" @click="changeFilter('all')">All</a></li>
+          <li><a href="#/active" :class="{selected: filter === 'active'}" @click="changeFilter('active')">Active</a></li>
+          <li><a href="#/completed" :class="{selected: filter === 'completed'}" @click="changeFilter('completed')">Completed</a></li>
         </ul>
-        <button class="clear-completed">Clear completed</button>
+        <button class="clear-completed" v-show="todos.some(data => data.completed)" @click="removeCompleted" >Clear completed</button>
       </footer>
     </section>
     <footer class="info">
@@ -51,6 +51,8 @@ import '@/assets/index.css'
 export default {
   data() {
     return {
+      filter: 'all',
+      newTodo: '',
       beforeEditCache:null,
       editTodo:null,
       todos:[{
@@ -63,6 +65,24 @@ export default {
     }
   },
   methods: {
+    changeFilter: function(filter) {
+      this.filter = filter
+    },
+    addTodo: function () {
+      var value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({ id: this.todos.length + 1, title: value, completed: false });
+      this.newTodo = '';
+    },
+    removeTodo: function (todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    removeCompleted: function () {
+      this.todos = this.todos.filter(data => !data.completed);
+    },
     editingMode(todo) {
       this.beforeEditCache = todo.title;
       this.editTodo = todo;
@@ -78,7 +98,6 @@ export default {
         this.removeTodo(todo);
       }
     },
-
     cancelEdit: function (todo) {
       this.editTodo = null;
       todo.title = this.beforeEditCache;
@@ -86,6 +105,19 @@ export default {
     },
   },
   computed: {
+      showTodo: function() {
+        let filter = this.filter;
+        let showTodo = this.todos.filter( data => {
+          if(filter === 'all'){
+            return data
+          }else if( filter === 'active'){
+            return !data.completed
+          }else if( filter === 'completed'){
+            return data.completed
+          }
+        })
+        return showTodo
+      },
       allDone: {
         get: function () {
           return this.todos.every(data => data.completed);
